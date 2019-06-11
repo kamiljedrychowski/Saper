6cdf502d39d418e7da66b12a8af75f4a4a981f84
kamiljedrychowski <kamil1998521@gmail.com>
2019-06-11 20:44:07 +0200

    Imports for java fx

--- src/main/java/packagee/Main.java
+++ src/main/java/packagee/Main.java
@@ -1,4 +1,28 @@
 package packagee;
 
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseButton;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
+    
 }
